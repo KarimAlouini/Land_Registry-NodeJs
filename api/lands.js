d6a1c1eb148d7f81b777efde226cd1f8d578b1fd
userKarim <alouini.karim@gmail.com>
2018-04-02 12:07:21 +0100

    new fixx

--- api/lands.js
+++ api/lands.js
@@ -304,23 +304,7 @@ function getLogsFromCache(){
 }
 
 
-router.get('/', function (req, res) {
-    Lands.find({}, function (err, data) {
-        if (err)
-            res.status(500).send();
-        else {
-            res.json(data);
-        }
-    })
-});
-router.post('/add', function (req, res) {
-    var new_land = new Lands(req.body);
-    new_land.save(function (err, data) {
-        if (err)
-            res.send(err);
-        res.json(data);
-    })
-});
+
 
 
 module.exports = router;
