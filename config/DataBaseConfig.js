f944401d35ceb10ec1fb40c7f363822b3e555c24
ahmed <ahmed.benrouha@esprit.tn>
2018-04-03 21:01:36 +0200

    fix for connection string

--- config/DataBaseConfig.js
+++ config/DataBaseConfig.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 //change to local database if you run into any problems :)
 
-//var connection = mongoose.connect('mongodb://quadra:quadra@34.252.123.72:27017/land_registry');
-var connection = mongoose.connect('mongodb://127.0.0.1/land_registry');
+var connection = mongoose.connect('mongodb://quadra:quadra@34.252.123.72:27017/land_registry');
+//var connection = mongoose.connect('mongodb://127.0.0.1/land_registry');
 
 
 
