6c6cbdac4005da05aef6a2aa8884335782b95891
nadim <mohamednadim.matoussi@esprit.tn>
2018-04-27 05:54:37 +0100

    added require user and agent middlewares

--- api/users.js
+++ api/users.js
@@ -5,33 +5,32 @@ var User = require('../models/User.model');
 const tokensUtil = require('../utils/tokens');
 router.post('/', function (req, res) {
 
+
     var login = req.body.login;
     var pwd = req.body.pwd;
     console.log('here.log');
-    User.findOne({login: login}, function (err, result) {
+    User.findOne({login: login, password: md5(pwd)}, function (err, result) {
 
         if (err)
             res.status(401).send();
 
-            if (result == null) {
-                res.status(401).send();
-            }
-            else {
-                tokensUtil.generateToken(result.login, (token) => {
-                    res.json({
-                        userInfo: result,
-                        token
-                    });
+        if (result == null) {
+            res.status(401).send();
+        }
+        else {
+            tokensUtil.generateToken(result.login, (token) => {
+                res.json({
+                    userInfo: result,
+                    token
                 });
-            }
-
-
-
-
+            });
+        }
     });
 
+
 });
-router.get('/info/:id', (req, res) => {
+router.get('/info/:id', tokensUtil.requireAgent,(req, res,next) => {
+    console.log(req);
     User.findOne({_id: req.params.id}, {password: 0}, (err, result) => {
         if (err)
             res.status(401).send();
--- utils/tokens.js
+++ utils/tokens.js
@@ -16,7 +16,7 @@ module.exports.readToken = (req) => {
 module.exports.decodeToken = (token) => {
 
     try {
-        let decoded = jwt.verify(token, 'xx');
+        let decoded = jwt.verify(token, constants.jwtSecret);
         return decoded;
 
     } catch (error) {
@@ -24,10 +24,10 @@ module.exports.decodeToken = (token) => {
     }
 };
 
-module.exports.generateToken = (login,callback) => {
-    User.findOne({login},(errr,user)=>{
+module.exports.generateToken = (login, callback) => {
+    User.findOne({login}, (errr, user) => {
 
-        jwt.sign(_.pick(user,'login','role','_id'), constants.jwtSecret, {expiresIn: '1h'}, function (err, token) {
+        jwt.sign(_.pick(user, 'login', 'role', '_id'), constants.jwtSecret, {expiresIn: '1h'}, function (err, token) {
 
             if (!err)
                 callback(token);
@@ -35,4 +35,42 @@ module.exports.generateToken = (login,callback) => {
         });
     });
 
+};
+
+module.exports.requireAgent = (req, res, next) => {
+    let decodedToken = module.exports.decodeToken(module.exports.readToken(req));
+    if (decodedToken == null) {
+        res.status(403).send();
+
+    }
+    else {
+        if (decodedToken.role == 'Agent') {
+            req.userId = decodedToken._id;
+            next();
+        }
+        else {
+            res.status(403).send();
+
+        }
+    }
+
+};
+module.exports.requireUser = (req, res, next) => {
+    let decodedToken = module.exports.decodeToken(module.exports.readToken(req));
+    if (decodedToken == null) {
+        res.status(403).send();
+
+    }
+    else {
+        if (decodedToken.role == 'User' || decodedToken.role == 'Agent') {
+            req.userId = decodedToken._id;
+            next();
+
+        }
+
+        else {
+            res.status(403).send();
+
+        }
+    }
 };
